296aab4509ba381619eab4001880ea1a63dff4f8
tbornino <vbornino@calpoly.edu>
2022-02-03 11:04:16 -0800

    Lab 3 Main Page Implementation

---
+++ src/mainpage.py
@@ -0,0 +1,27 @@
+'''!@file       mainpage.py
+    Main page for Lab 3: Task Scheduler Implementation
+
+    @mainpage
+
+    @section sec_intro  Introduction
+                        This is the documentation site for Tori Bornino,
+                        Jackson Mclaughlin, and Zachary Stednitz's code used in
+                        ME 405 - Mechatronics, Lab 3: Task Scheduler  
+                        Implementation, taught in Winter 2022 by Dr. John 
+                        Ridgly. Source code is available at:
+                        https://tbornino.github.io/ME405lab3/
+    
+    @section sec_sch    Task Scheduler in Main
+                        We were supplied with a cotask.py to implement
+                        a scheduler to run our step response. The 
+                        @ref motor.MotorDriver "Motor Driver", 
+                        @ref encoder.EncoderDriver "Encoder Driver", and
+                        @ref pidcontroller.PIDController "PID Controller"
+                        were developed in Lab 2 whose documentation can be
+                        found at: https://tbornino.github.io/ME405lab2/
+                         
+    @author             Tori Bornino
+    @author             Jackson McLaughlin
+    @author             Zach Stednitz
+    @date               February, 10 2022
+'''
